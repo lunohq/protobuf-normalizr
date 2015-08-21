6b04fa0f21be36f8064f4051064b388677a9f9ab
Michael Hahn <mwhahn@gmail.com>
2015-08-21 04:13:48 -0700

    First pass at denormalizing protobufs

---
+++ src/index.js
@@ -0,0 +1,130 @@
+const isEntity = (obj, key=null) => {
+    if (
+        (obj.hasOwnProperty('id') && obj.id !== null) ||
+        (key !== null)
+    ) {
+        return true;
+
+    }
+    return false;
+}
+
+const getEntityKey = (entity) => {
+    return entity.$type.name.toLowerCase();
+}
+
+const getEntityId = (entity, key=null) => {
+    return key ? key : entity.get('id');
+}
+
+const normalizeField = (field, protobuf, protobufs, normalizations, key=null) => {
+    let value = protobuf.get(field.name)
+    if (!(field.repeated || isEntity(value, key))) {
+        return;
+    }
+
+    let entityKey = getEntityKey(protobuf);
+    let entityId = getEntityId(protobuf, key);
+    if (!normalizations[entityKey]) {
+        normalizations[entityKey] = {};
+    }
+
+    if (!normalizations[entityKey][entityId]) {
+        normalizations[entityKey][entityId] = {};
+    }
+
+    let stored = normalizations[entityKey][entityId];
+    if (field.repeated && value !== null) {
+        if (!stored[field.name]) {
+            stored[field.name] = [];
+        }
+        value.map((childProtobuf) => {
+            stored[field.name].push(childProtobuf.id);
+            visit(childProtobuf, protobufs, normalizations);
+        })
+        protobuf.set(field.name, null);
+    } else if (value !== null && isEntity(value)) {
+        stored[field.name] = value.id;
+        visit(value, protobufs, normalizations);
+        protobuf.set(field.name, null);
+    }
+}
+
+const visitProtobuf = (protobuf, protobufs, normalizations, key=null) => {
+    protobuf.$type.children.map((field) => {
+        normalizeField(field, protobuf, protobufs, normalizations, key);
+    });
+    return protobuf;
+}
+
+const visitArray = (obj, protobufs, normalizations) => {
+    const normalized = obj.map((childObj) => {
+        return visit(childObj, protobufs, normalizations);
+    })
+    return normalized;
+}
+
+const visitEntity = (entity, protobufs, normalizations, key=null) => {
+    const entityKey = getEntityKey(entity);
+    const entityId = getEntityId(entity, key);
+
+    if (!protobufs[entityKey]) {
+        protobufs[entityKey] = {};
+    }
+
+    protobufs[entityKey][entityId] = entity;
+    visitProtobuf(entity, protobufs, normalizations, key);
+    return entityId;
+}
+
+const visit = (obj, protobufs, normalizations, key=null) => {
+    if (obj.$type && isEntity(obj, key)) {
+        return visitEntity(obj, protobufs, normalizations, key);
+    } else if (obj.$type) {
+        return visitProtobuf(obj, protobufs, normalizations);
+    } else if (obj instanceof Array) {
+        return visitArray(obj, protobufs, normalizations);
+    }
+
+    return obj;
+}
+
+const denormalizeProtobuf = (protobuf, entityKey, key, state) => {
+    if (!state.normalizations[entityKey]) {
+        return;
+    }
+
+    const fieldNames = protobuf.$type._fieldsByName;
+    const normalizations = state.normalizations[entityKey][key];
+    for (let field in normalizations) {
+        const value = normalizations[field];
+        const type = fieldNames[field].resolvedType.name.toLowerCase();
+        if (value instanceof Array) {
+            protobuf.set(field, []);
+            value.map((id) => {
+                protobuf[field].push(state.protobufs[type][id]);
+            });
+        } else {
+            protobuf.set(field, state.protobufs[type][value]);
+        }
+    }
+}
+
+export const normalize = (obj, key=null) => {
+    let protobufs = {};
+    let normalizations = {};
+    let result = visit(obj, protobufs, normalizations, key);
+    return { protobufs, normalizations, result };
+}
+
+export const denormalize = (key, builder, state) => {
+    const entityKey = getEntityKey(builder);
+    if (!state.protobufs[entityKey]) {
+        return;
+    }
+    const protobuf = state.protobufs[entityKey][key];
+    denormalizeProtobuf(protobuf, entityKey, key, state)
+    return protobuf;
+}
+
+export default normalize;
---
+++ test/index.js
@@ -0,0 +1,208 @@
+import { should } from 'chai';
+import Protobuf from 'protobufjs';
+
+import normalize, { denormalize } from '../lib/index';
+
+should();
+
+const builder = Protobuf.loadProtoFile('./test/sample.proto');
+
+const mockProto = (builder, className, parameters) => {
+    const ProtobufBuilder = builder.build(className);
+    return new ProtobufBuilder(parameters);
+}
+
+const mockStatus = (builder, parameters={}) => {
+    const defaults = {
+        value: 'status value',
+    }
+    return mockProto(builder, 'Status', Object.assign({}, defaults, parameters));
+}
+
+const mockProfile = (builder, parameters={}, statusParameters={}) => {
+    const defaults = {
+        id: 1,
+        name: 'Name',
+        title: 'Title',
+        status: mockStatus(builder, statusParameters),
+    }
+    return mockProto(
+        builder,
+        'Profile',
+        Object.assign({}, defaults, parameters)
+    );
+}
+
+const mockAddress = (builder, parameters={}) => {
+    const defaults = {
+        id: 1,
+        street: 'Market',
+        city: 'SF',
+        state: 'CA'
+    };
+
+    return mockProto(
+        builder,
+        'Address',
+        Object.assign({}, defaults, parameters)
+    );
+}
+
+const mockMultipleProfileResponse = (builder, parameters={}) => {
+    const defaults = {
+        profiles: [mockProfile(builder, {id: 1}), mockProfile(builder, {id: 2})],
+    }
+    return mockProto(
+        builder,
+        'MultipleProfileResponse',
+        Object.assign({}, defaults, parameters)
+    );
+}
+
+const mockLocation = (
+    builder,
+    parameters={},
+    addressParameters={},
+    adminParameters={id: 1},
+    profileParameters={id: 2}
+) => {
+    const defaults = {
+        id: 1,
+        name: 'HQ',
+        address: mockAddress(builder, addressParameters),
+        admins: [mockProfile(builder, adminParameters)], 
+        profiles: [mockProfile(builder, profileParameters)],
+    }
+
+    return mockProto(
+        builder,
+        'Location',
+        Object.assign({}, defaults, parameters)
+    );
+}
+
+describe('protobuf-normalizr', () => {
+
+    describe('normalize', () => {
+
+        it('can normalize a single entity', () => {
+            const address = mockAddress(builder);
+            normalize(address).should.eql({
+                result: 1,
+                protobufs: {
+                    address: {
+                        1: address,
+                    },
+                },
+                normalizations: {},
+            });
+        });
+
+        it('can normalize an array of single entities', () => {
+            const [address1, address2] = [
+                mockAddress(builder),
+                mockAddress(builder, {id: 2}),
+            ];
+            normalize([address1, address2]).should.eql({
+                result: [1, 2],
+                protobufs: {
+                    address: {
+                        1: address1,
+                        2: address2,
+                    },
+                },
+                normalizations: {},
+            });
+        });
+
+        it('can normalize an entity with a nested non-entity protobuf', () => {
+            const profile = mockProfile(builder);
+            normalize(profile).should.eql({
+                result: 1,
+                protobufs: {
+                    profile: {
+                        1: profile,
+                    },
+                },
+                normalizations: {},
+            });
+        });
+
+        it('can normalize an entity with nested entities', () => {
+            const location = mockLocation(builder);
+            const expected = location.$type.clazz.decode(location.encode());
+            normalize(location).should.eql({
+                result: 1,
+                protobufs: {
+                    profile: {
+                        1: expected.admins[0],
+                        2: expected.profiles[0],
+                    },
+                    location: {
+                        1: location,
+                    },
+                    address: {
+                        1: expected.address,
+                    },
+                },
+                normalizations: {
+                    location: {
+                        1: {
+                            profiles: [2],
+                            admins: [1],
+                            address: 1,
+                        },
+                    }
+                },
+            });
+        });
+
+        it('can normalize a message with nested entities', () => {
+            const response = mockMultipleProfileResponse(builder);
+            const expected = response.$type.clazz.decode(response.encode());
+            const key = 'key';
+            normalize(response, key)
+                .should.eql({
+                    result: key,
+                    protobufs: {
+                        profile: {
+                            1: expected.profiles[0],
+                            2: expected.profiles[1],
+                        },
+                        multipleprofileresponse: {
+                            key: response,
+                        },
+                    },
+                    normalizations: {
+                        multipleprofileresponse: {
+                            key: {
+                                profiles: [1, 2],
+                            },
+                        },
+                    },
+                });
+        });
+
+    });
+
+    describe('denormalize', () => {
+
+        it('can denormalize a normalized response', () => {
+            const response = mockMultipleProfileResponse(builder);
+            const expected = response.$type.clazz.decode(response.encode());
+            const key = 'key';
+            const state = normalize(response, key);
+            denormalize(key, builder.build('MultipleProfileResponse'), state)
+                .should.eql(expected);
+        })
+
+        it('can denormalize a single entity', () => {
+            const address = mockAddress(builder);
+            const state = normalize(address);
+            denormalize(state.result, builder.build('Address'), state)
+                .should.eql(address);
+
+        });
+    });
+
+})
